af2903284e0c789dfad9fc658dbf511c6b4896bc
Iftekher Aziz <azizchy02@gmail.com>
2023-05-17 09:27:11 +0600

    commit

---
+++ car-doctor-client-jwt/src/Routes/PrivateRoute.jsx
@@ -0,0 +1,28 @@
+import { useContext } from "react";
+import { AuthContext } from "../Providers/AuthProvider";
+import { Navigate, useLocation } from "react-router-dom";
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
+
+  if (loading) {
+    return (
+      <div className="text-center">
+        <progress className="progress w-56"></progress>
+      </div>
+    );
+  }
+
+  if (user?.email) {
+    return children;
+  }
+
+  return (
+    <div>
+      <Navigate to="/login" state={{ from: location }} replace></Navigate>
+    </div>
+  );
+};
+
+export default PrivateRoute;
